7d139960afa41e201c0578dadaf4b414f2026416
HammIamm <ramadhanfaturriham@gmail.com>
2025-02-13 11:51:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,18 +31,21 @@ function typeWriter() {
 
 // *Slideshow Background*
 const images = [
-    "photo1.jpg", "photo2.jpg", "photo3.jpg", "photo4.jpg", "photo5.jpg",
-    "photo6.jpg", "photo7.jpg", "photo8.jpg", "photo9.jpg", "photo10.jpg",
-    "photo11.jpg", "photo12.jpg", "photo13.jpg", "photo14.jpg", "photo15.jpg",
-    "photo16.jpg", "photo17.jpg"
+    "images/photo1.jpg", "images/photo2.jpg", "images/photo3.jpg", "images/photo4.jpg", "images/photo5.jpg",
+    "images/photo6.jpg", "images/photo7.jpg", "images/photo8.jpg", "images/photo9.jpg", "images/photo10.jpg",
+    "images/photo11.jpg", "images/photo12.jpg", "images/photo13.jpg", "images/photo14.jpg", "images/photo15.jpg",
+    "images/photo16.jpg", "images/photo17.jpg"
 ];
 
 let currentIndex = 0;
 
 function changeBackground() {
-    document.body.style.backgroundImage = url('./images/${images[currentIndex]}');
+    let imagePath = images[currentIndex];
+    console.log("Changing background to:", imagePath); // Debugging biar tau path gambar bener atau enggak
+    document.body.style.backgroundImage = url('${imagePath}');
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundAttachment = "fixed"; // Biar background gak gerak pas scroll
     currentIndex = (currentIndex + 1) % images.length;
 }
 
